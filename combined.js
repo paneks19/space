1da162645d9a3c3ad84f2758bcd5a79e599d15e6
Simon Panek <paneks19@gmail.com>
2020-09-22 18:43:30 -0700

    Added loop controlled password at website entry

--- combined.js
+++ combined.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var correctCode = 'C0d3';
+
+for(var i = 0; i < 5; i++){
+    var userCode = prompt("Please enter the initialization code for the space probe:  Hint (C0d3)");
+    if (userCode === correctCode){
+        alert('Initiation Sequence Commencing!')
+        i = 5;
+    } 
+    if (userCode !== correctCode && i == 4){
+        alert('You have failed to start the space probe in the alloted number of attemtps. It has entered a decaying orbit and will now burn up in the atmosphere.')
+    }
+}
+
 function askUserName(){
     var userName = prompt('Please enter your name');
     if (userName == 'Simon') {
@@ -42,7 +55,7 @@ function marsWeight(){
     var userWeightEarth = prompt('How many pounds do you weigh on Earth?')
     if (userWeightEarth % 1 == 0){
         var userWeightMars = userWeightEarth * (0.376);
-        document.write('Thank you for sharing your personal informatin, it it will not be sold! On Mars you would weigh ' + userWeightMars + ' pounds.')
+        document.write('Thank you for sharing your personal information, it it will not be sold! On Mars you would weigh ' + userWeightMars + ' pounds.')
         return userWeightEarth;
     } else {
         document.write('You have entered an incorrect value and broken my space probe. Once the probe has developed an understanding of Loops you can try again.')
@@ -50,3 +63,43 @@ function marsWeight(){
     
 }
 
+
+// Following class 8 video
+
+// While (x > 10 || x<1){
+//     x = prompt('Enter a number between 1 and 10')
+// }
+
+// for (var i = 0; i <= 12; i = i + 1) {
+//     console.log('The number is: ' + 1);
+// }
+
+// var password = 'abcd';
+
+// var correctPassword = prompt('Enter password:');
+
+// while(correctPassword !== password) {
+//     for(i = 0; i < 5; i++){
+//         correctPassword = prompt('Try again, enter password: ');
+
+
+//     }
+// }
+
+// for(var i = 0; i <5; i++){
+//     var correctPassword = prompt('Enter password: ')
+//     console.log(i);
+//     if (correctPassword === password){
+//         alert('You entered the correct password.')
+//         i = 5; //or could use `break;`
+//     }
+//     if (i === 4 && correctPassword 1+ password){
+//         alert('You have been locked out for 5 minutes')
+//     }
+// }
+
+// while (correctPassword !== password){
+//     correctPassword = prompt ('Try again.');
+// }
+
+//guess a number between 1 and 100 then give x chances to get it correct
